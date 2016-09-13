b124f1ca18810f3c100b1aa4e4dc19baa99e77da
myer <ye.medetov@ecc.kz>
2016-09-13 19:29:04 +0600

    added thankYou page

--- proangular/angularjs/controllers/sportsStore.js
+++ proangular/angularjs/controllers/sportsStore.js
@@ -1,7 +1,7 @@
 angular.module("sportsStore")
 .constant("dataUrl", "http://localhost:5500/products")
 .constant("orderUrl", "http://localhost:5500/orders")
-.controller("sportsStoreCtrl", function ($scope, $http, dataUrl, orderUrl, cart) {
+.controller("sportsStoreCtrl", function ($scope, $http, $location, dataUrl, orderUrl, cart) {
 $scope.data = {};
 
 $http.get(dataUrl)
@@ -13,18 +13,18 @@ $scope.data.error = error;
 });
 
 $scope.sendOrder = function (shippingDetails) {
-var order = angular.copy(shippingDetails);
-order.products = cart.getProducts();
-$http.post(orderUrl, order)
-.success(function (data) {
-$scope.data.orderId = data.id;
-cart.getProducts().length = 0;
-})
-.error(function (error) {
-$scope.data.orderError = error;
-}).finally(function () {
-$location.path("/complete");
-});
-}
+            var order = angular.copy(shippingDetails);
+            order.products = cart.getProducts();
+            $http.post(orderUrl, order)
+                .success(function (data) {
+                    $scope.data.orderId = data.id;
+                    cart.getProducts().length = 0;
+                })
+                .error(function (error) {
+                    $scope.data.orderError = error;
+                }).finally(function () {
+                    $location.path("/complete");
+                });
+        }
 
 });

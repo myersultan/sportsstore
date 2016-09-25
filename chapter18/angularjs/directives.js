caaf8258c1d3b543a1b73ad290efed30f277d0fa
myersultan <y.medetov@gmail.com>
2016-09-25 23:12:16 +0600

    chapter18

---
+++ chapter18/angularjs/directives.js
@@ -0,0 +1,14 @@
+angular.module("customDirectives", ["customDirectives"])
+.directive("triButton", function (logService) {
+	return {
+		scope: { counter: "=counter" },
+		link: function (scope, element, attrs) {
+			element.on("click", function (event) {
+				logService.log("Button click: " + event.target.innerText);
+				scope.$apply(function () {
+					scope.counter++;
+				});
+			});
+		}
+	}
+});

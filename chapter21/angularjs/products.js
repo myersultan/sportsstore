27ab8497d8f6578ae44fd6021dbe9f9abddd17fa
myersultan <y.medetov@gmail.com>
2016-10-12 02:31:25 +0600

    ch22

--- chapter21/angularjs/products.js
+++ chapter21/angularjs/products.js
@@ -1,5 +1,9 @@
 angular.module("exampleApp", ["increment", "ngResource", "ngRoute"])
 .constant("baseUrl", "http://localhost:5500/products/")
+.factory("productsResource", function($resource, baseUrl) {
+	return $resource(baseUrl + ":id", {id: "@id"},
+				{ create: { method: "POST" }, save: { method: "PUT" }});
+})
 .config(function ($routeProvider, $locationProvider){
 
 	$locationProvider.html5Mode({
@@ -10,58 +14,80 @@ angular.module("exampleApp", ["increment", "ngResource", "ngRoute"])
 
 	$locationProvider.html5Mode(true);
 
-
-	$routeProvider.when("/list", {
-		templateUrl: "/tableView.html"
-	});
-
-	$routeProvider.when("/edit", {
-		templateUrl: "/editorView.html"
+	$routeProvider.when("/edit/:id", {
+		templateUrl: "/editorView.html",
+		controller: "editCtrl"
 	});
 
 	$routeProvider.when("/create", {
-		templateUrl: "/editorView.html"
+		templateUrl: "/editorView.html",
+		controller: "editCtrl"
 	});
 
 	$routeProvider.otherwise({
-		templateUrl: "/tableView.html"
+		templateUrl: "/tableView.html",
+		controller: "tableCtrl",
+		resolve: {
+			data: function(productsResource){
+				return productsResource.query();
+			}
+		}
 	});
 })
-.controller("defaultCtrl", function ($scope, $http, $resource, $location, baseUrl) {
-	
+.controller("defaultCtrl", function ($scope, $location, productsResource) {
 
-
-	$scope.currentProduct = null;
-
-	$scope.productsResource = $resource(baseUrl + ":id", {id: "@id"},
-				{ create: { method: "POST" }, save: { method: "PUT" }});
-								
-
-	$scope.listProducts = function () {		
-			$scope.products = $scope.productsResource.query();		
-	}
+	$scope.data = {};
 
 	$scope.deleteProduct = function (product) {
 		
 			product.$delete().then(function(){
-				$scope.products.splice($scope.products.indexOf(product), 1);
+				$scope.data.products.splice($scope.data.products.indexOf(product), 1);
 			});
 			$location.path("/list");		
 	}
+
 	$scope.createProduct = function (product) {
-		new $scope.productsResource(product).$create().then(function(newProduct){
-			$scope.products.push(newProduct);
+		new productsResource(product).$create().then(function(newProduct){
+			$scope.data.products.push(newProduct);
 			$location.path("/list");
 		});	
+	}	
+	
+	
+})
+
+.controller("tableCtrl", function($scope, $location, $route, data) {
+	$scope.data.products = data;
+
+	$scope.refreshProducts = function() {
+		$route.reload();
 	}
+})
+
+.controller("editCtrl", function($scope, $routeParams, $location) {
+
+	$scope.currentProduct = null;
+
+	if ($location.path().indexOf("/edit/") == 0) {
+		var id = $routeParams["id"];
+		for (var i = 0; i < $scope.data.products.length; i++) {
+			if ($scope.data.products[i].id == id) {
+				$scope.currentProduct = $scope.data.products[i];
+				break;
+			}
+		}
+	}
+
+
+	$scope.cancelEdit = function () {
+		$location.path("/list");
+	}
+
 	$scope.updateProduct = function (product) {
 		product.$save();
 		$location.path("/list");		
 	}
-	$scope.editProduct = function (product) {
-		$scope.currentProduct = product;
-		$location.path("/edit");
-	}
+
 	$scope.saveEdit = function (product) {
 		if (angular.isDefined(product.id)) {
 			$scope.updateProduct(product);
@@ -71,12 +97,5 @@ angular.module("exampleApp", ["increment", "ngResource", "ngRoute"])
 		$scope.currentProduct = {};
 
 	}
-	$scope.cancelEdit = function () {
-		if ($scope.currentProduct && $scope.currentProduct.$get) {
-			$scope.currentProduct.$get();
-		}
-		$scope.currentProduct = {};
-		$location.path("/list");
-	}
-	$scope.listProducts();
+
 });
